d9876b01d4e039e2eeabc80178048e441f5a4ad8
r1ch <rich@bradi.sh>
2025-09-23 14:11:31 +0100

    Create recipes.js

---
+++ recipes.js
@@ -0,0 +1,24 @@
+---
+layout: none
+---
+  <script>
+  const ibR = {}
+  const r = {}
+  {%- for recipe in site.recipes -%}
+    {%- assign recipe_data = site.data[recipe.slug] -%}
+    {% if recipe_data %}
+  r["{{recipe.slug}}"] = {"title":"{{recipe.title}}","url":"{{recipe.url}}","totalTime":"{{recipe_data.totalTime | replace: "PT", "" | replace: "M", " minutes"  }}"};
+  ibR["{{recipe.slug}}"] = [
+      {%- assign ingredients = recipe_data.ingredients -%}
+      {%- assign yields = recipe_data.yields -%}
+        {%- for yield in yields -%}
+          {%- if yield.yields == 2 -%}
+            {%- for ing in yield.ingredients -%}
+              {% assign ingredient = ingredients | where: "id", ing.id | first %}
+              {"type":"{{ingredient.type}}", "name":"{{ingredient.name}}", "amount":"{{ing.amount}}", "unit":"{{ing.unit}}"},
+            {%- endfor -%}
+          {%- endif -%}
+        {%- endfor -%}
+  ];
+    {%- endif -%}
+  {%- endfor -%}
