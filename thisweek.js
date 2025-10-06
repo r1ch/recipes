f123cf75c4c12b28126bb3b976d038e877312e80
r1ch <rich@bradi.sh>
2025-10-06 12:53:36 +0100

    Update thisweek.js

--- thisweek.js
+++ thisweek.js
@@ -1,6 +1,6 @@
 ---
 layout: none
 ---
-export thisWeek = ['21-day-aged-rump-steak-and-chimichurri-sauce-6867921e700c002ea14ffae0',
+export const thisWeek = ['21-day-aged-rump-steak-and-chimichurri-sauce-6867921e700c002ea14ffae0',
                    'pesto-baked-salmon-and-oregano-butternut-squash-67d1b8ffe4321b3b6ef33a96',
                    'serrano-topped-chicken-breast-68937b1c464ec79ed46ff6db']
