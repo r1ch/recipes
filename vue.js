03e0f253958bbcb08d8ea4faeef7a44c9f3b25fc
r1ch <rich@bradi.sh>
2025-10-09 21:05:40 +0100

    Update vue.js

--- vue.js
+++ vue.js
@@ -7,8 +7,9 @@ const { createApp, ref, computed } = Vue
   const app = createApp({
       setup() {
         const queryParams = new URLSearchParams(window.location.search)
-        const pickedRaw = window.location.pathname.includes("thisweek") ?
-            (queryParams.getAll('r').length > 0 ? queryParams.getAll('r') : thisWeek) : []
+        const thisWeekPage = window.location.pathname.includes("thisweek")
+        const loadedFromQuery = queryParams.getAll('r').length > 0
+        const pickedRaw = thisWeekPage ? (loadedFromQuery ? queryParams.getAll('r') : thisWeek) : []
         const picked = ref(Object.keys(recipes).filter(id=>pickedRaw.includes(id)))
         const queryString = computed(()=>picked.value.map(v=>`r=${v}`).join("&"))
         const thisWeekLink = computed(()=>{
