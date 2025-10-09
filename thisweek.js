3cd1e6700589e2ee4a4107c1c4383c6c49ee3294
r1ch <rich@bradi.sh>
2025-10-09 21:44:16 +0100

    Update thisweek.js via Google Apps Script

--- thisweek.js
+++ thisweek.js
@@ -1,4 +1,4 @@
 ---
 layout: none
 ---
-export const thisWeek = ["miso-glazed-salmon-60ed8af58452130fbf4e14b6","lamb-steak-and-biryani-inspired-rice-6880dc467299475a5b062d94","beef-chilli-con-carne-68874443637c1fda3ad8acdf"]
+export const thisWeek = ["beef-chilli-con-carne-68874443637c1fda3ad8acdf","lamb-steak-and-biryani-inspired-rice-6880dc467299475a5b062d94","miso-glazed-salmon-60ed8af58452130fbf4e14b6"]
